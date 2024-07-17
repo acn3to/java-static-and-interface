fdd37558ea9285c9fcf44e1bd4f7cadb9f40741f
acn3to <costanetoow@gmail.com>
2024-07-16 23:26:57 -0300

    feat: exercise 6 finished

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,12 @@
-import bank.Bank;
-import bank.BankAccount;
+import print.Document;
+import print.Image;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount myAccount = Bank.openAccount("123456", "Arnaldo Neto", 1000.00);
-        System.out.println("Conta aberta com sucesso: " + myAccount);
+        Document document = new Document();
+        Image image = new Image();
 
-        Bank.deposit("123456", 500.00);
-        System.out.println("Depósito realizado com sucesso. Saldo atualizado: " + myAccount.getBalance());
-
-        boolean success = Bank.withdraw("123456", 200.00);
-        System.out.println((success ? "Saque realizado com sucesso. Saldo atualizado: " + myAccount.getBalance() : "Não foi possível realizar o saque. Saldo insuficiente."));
-
-        boolean transferSuccess = Bank.transfer("123456", "789012", 300.00);
-        System.out.println((transferSuccess ? "Transferência realizada com sucesso. Saldo da conta de origem atualizado: " + myAccount.getBalance() : "Falha na transferência. Verifique os números das contas e o saldo disponível."));
-
-        Bank.closeAccount("123456");
-        System.out.println("Conta fechada com sucesso.");
-
-        System.out.println("Contas bancárias abertas:");
-        Bank.listAccounts();
+        document.print();
+        image.print();
     }
 }
---
+++ src/print/Image.java
@@ -0,0 +1,8 @@
+package print;
+
+public class Image implements Printable {
+    @Override
+    public void print() {
+        System.out.println("Imprimindo imagem...");
+    }
+}
