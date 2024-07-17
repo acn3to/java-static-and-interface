76d09dc2aea10b2de5f2ce0ffa33d3022e501295
acn3to <costanetoow@gmail.com>
2024-07-16 23:38:32 -0300

    feat: exercise 8 finished

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
-import speak.Cat;
-import speak.Dog;
+import sort.BubbleSort;
+import sort.QuickSort;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        Cat cat = new Cat();
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
 
-        dog.speak();
-        cat.speak();
+        System.out.println("Array original:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.sort(array);
+        System.out.println("Array ordenado com Bubble Sort:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        QuickSort quickSort = new QuickSort();
+        quickSort.sort(array);
+        System.out.println("Array ordenado com Quick Sort:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
     }
 }
